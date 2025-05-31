ecdfcdae4bad71e8df9a2ef9c581e68584994489
Paula <paulabomp@gmail.com>
2025-05-31 19:21:16 -0300

    ABM Modelo Inventario FUNCIONANDO 100% real no fake
    ABM ModeloInventario terminado
    
    Clases agregadas/modificadas:
    
    -DTOMI
    -CONTROLADOR MI
    -EXPERTO MI
    -CLASE MI
    -REPOSITORIO MI

---
+++ src/main/java/com/invOperativa/Integrador/CU/CU17_ABMModeloInventario/DTOABMModeloInventario.java
@@ -0,0 +1,24 @@
+package com.invOperativa.Integrador.CU.CU17_ABMModeloInventario;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DTOABMModeloInventario {
+
+    private Long idMI;
+
+    private Date fhBajaMI;
+
+    private String nombreMI;
+
+    private boolean MIdadoBaja;
+
+}
