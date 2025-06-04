2945d91033294a18e1a9322fd44ab5b7990f2d64
Ramiro <deconinckramiro@gmail.com>
2025-06-04 18:04:44 -0300

    CU ListarArticuloXProveedor

---
+++ src/main/java/com/invOperativa/Integrador/CU/CU11_ListarArticuloXProveedores/DTOArticuloProv.java
@@ -0,0 +1,30 @@
+package com.invOperativa.Integrador.CU.CU11_ListarArticuloXProveedores;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DTOArticuloProv {
+
+    private Long idArticulo;
+
+    private String modeloInventario;
+
+    private Boolean isPredeterminado;
+
+    private String descripcionArticulo;
+
+    private float precioUnitario;
+
+    private float costoUnitario;
+
+    private int demoraEntrega;
+
+    private float costoPedido;
+
+}
