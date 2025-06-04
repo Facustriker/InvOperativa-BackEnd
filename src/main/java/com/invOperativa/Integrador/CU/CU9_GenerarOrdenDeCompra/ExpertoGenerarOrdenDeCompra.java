54a3ef02e2c4bd19d0487a44fa5769ef45da53e1
Ramiro <deconinckramiro@gmail.com>
2025-06-03 23:21:28 -0300

    Correcciones version anterior

--- src/main/java/com/invOperativa/Integrador/CU/CU9_GenerarOrdenDeCompra/ExpertoGenerarOrdenDeCompra.java
+++ src/main/java/com/invOperativa/Integrador/CU/CU9_GenerarOrdenDeCompra/ExpertoGenerarOrdenDeCompra.java
@@ -1,5 +1,10 @@
 package com.invOperativa.Integrador.CU.CU9_GenerarOrdenDeCompra;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExpertoGenerarOrdenDeCompra {
 
     public void generacionAutomatica (Long id) {
