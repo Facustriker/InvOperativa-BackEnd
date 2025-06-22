4e3a612fd5d65fcfc81b9b22186075a64ae1e30b
Juan Pablo Masuet <juanpablomasuet@gmail.com>
2025-06-22 18:12:37 -0300

    cambios en calcelar OC

--- src/main/java/com/invOperativa/Integrador/CU/CU8_CancelarOrdenDeCompra/ControladorCancelarOrdenDeCompra.java
+++ src/main/java/com/invOperativa/Integrador/CU/CU8_CancelarOrdenDeCompra/ControladorCancelarOrdenDeCompra.java
@@ -1,10 +1,10 @@
 package com.invOperativa.Integrador.CU.CU8_CancelarOrdenDeCompra;
 
+import com.invOperativa.Integrador.CU.CU4_ModificarOrdenDeCompra.DTODatosModificacion;
+import com.invOperativa.Integrador.CU.CU4_ModificarOrdenDeCompra.DTOModificarOrdenCompra;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(path = "/CancelarOrdenDeCompra")
 public class ControladorCancelarOrdenDeCompra {
@@ -12,4 +12,15 @@ public class ControladorCancelarOrdenDeCompra {
     @Autowired
     protected ExpertoCancelarOrdenDeCompra experto;
 
+    @GetMapping("/getDatosOC")
+    public ResponseEntity<?> getDatosOC(@RequestParam Long idOC) {
+        DTOModificarOrdenCompra ret = experto.getDatosOC(idOC);
+        return ResponseEntity.ok(ret);
+    }
+
+    @PostMapping("/cancelarOrden")
+    public ResponseEntity<?> cancelarOrden(@RequestBody DTODatosModificacion dto) {
+        experto.cancelarOrden();
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/invOperativa/Integrador/CU/CU8_CancelarOrdenDeCompra/ExpertoCancelarOrdenDeCompra.java
+++ src/main/java/com/invOperativa/Integrador/CU/CU8_CancelarOrdenDeCompra/ExpertoCancelarOrdenDeCompra.java
@@ -1,4 +1,31 @@
 package com.invOperativa.Integrador.CU.CU8_CancelarOrdenDeCompra;
 
+import com.invOperativa.Integrador.CU.CU4_ModificarOrdenDeCompra.DTOModificarOrdenCompra;
+import com.invOperativa.Integrador.CU.CU9_GenerarOrdenDeCompra.DTOSalidaOrdenDeCompra;
+import com.invOperativa.Integrador.Config.CustomException;
+import com.invOperativa.Integrador.Entidades.OrdenCompra;
+import com.invOperativa.Integrador.Repositorios.RepositorioOrdenCompra;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class ExpertoCancelarOrdenDeCompra {
+
+    @Autowired
+    RepositorioOrdenCompra repositorioOrdenCompra;
+
+    public DTOModificarOrdenCompra getDatosOC(Long idOC) {
+        Optional<OrdenCompra> ordenCompra = repositorioOrdenCompra.obtenerOCVigentePorID(idOC);
+
+        DTOSalidaOrdenDeCompra
+
+        if (ordenCompra.isEmpty()) {
+            throw new CustomException("Error, no se encontró la orden de compra seleccionada");
+        }
+
+
+
+        return dto
+    }
+
 }
